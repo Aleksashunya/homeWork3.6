34b9ea6aa3fcd6eb93a0c5768b9b7f08d6ebaa2d
Sasha <pusmi.1987@gmail.com>
2021-08-14 13:32:56 +1000

    Initial Commit

---
+++ homeWork3.6/homeWork3_6App.swift
@@ -0,0 +1,17 @@
+//
+//  homeWork3_6App.swift
+//  homeWork3.6
+//
+//  Created by Александра Мельникова on 14.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct homeWork3_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
